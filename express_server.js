dea29683cedfbb90e2e4f9d21a6771242f74263d
Mingu Kim <mkim245@gmail.com>
2022-07-12 20:09:30 -0400

    updated

--- express_server.js
+++ express_server.js
@@ -32,6 +32,10 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL;
+});
+
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   res.send("Ok"); // Respond with 'Ok' (we will replace this)
@@ -40,6 +44,10 @@ app.post("/urls", (req, res) => {
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
+});
+
+app.post("/urls/:id/edit", (req, res) => {
+  res.redirect("/urls/" + req.params.id);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
