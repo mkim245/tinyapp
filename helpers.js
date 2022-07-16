131229e10aa2f24f9460d83cbaa51c0a835d872a
Mingu Kim <mkim245@gmail.com>
2022-07-16 15:09:03 -0400

    add comments on function use

--- helpers.js
+++ helpers.js
@@ -1,10 +1,11 @@
+// Checks if given ID corresponds to a user in a given database
 const getUserByID = function(userID, users) {
   if (users[userID]) {
     console.log(users[userID]);
     return users[userID];
   } return false;
 };
-
+// Checks if an email corresponds to a user in a given database
 const getUserByEmail = function(newEmail, users) {
   for (let user in users) {
     if (users[user].email === newEmail) {
@@ -13,7 +14,7 @@ const getUserByEmail = function(newEmail, users) {
   }
   return false;
 };
-
+// Takes an email and given database and returns the user ID for the user with the given email address
 const userIdFromEmail = function(email, users) {
   for (let user in users) {
     if (users[user].email === email) {
@@ -21,7 +22,7 @@ const userIdFromEmail = function(email, users) {
     }
   }
 };
-
+// Returns a URL corresponding to a submitted userID 
 const urlsForUser = function(id, urlDatabase) {
   const userUrls = {};
   for (let shortURL in urlDatabase) {
